a7abc9096ef7046dcef2206661a6debd70afb982
Viktor <viktor.malakhov05@gmail.com>
2020-11-24 17:59:05 +0500

    commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+from random import randint
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Circles(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Git и желтые окружности')
+        self.do_paint = False
+        self.draw_btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 200, 0))
+        cnt = randint(3, 100)
+        for i in range(cnt):
+            x, y = randint(0, self.width()), randint(0, self.height())
+            d = randint(5, 100)
+            qp.drawEllipse(x, y, d, d)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Circles()
+    ex.show()
+    sys.exit(app.exec())
